7589b2147271d587cb998d6a95a09a018dd68fea
larissanne <larissadavies31@gmail.com>
2019-05-30 11:22:55 +0100

    Refactored lambda function to work without API Gateway

--- src/main/java/uk/gov/ons/validation/serverless/Handler.java
+++ src/main/java/uk/gov/ons/validation/serverless/Handler.java
@@ -1,35 +1,30 @@
 package uk.gov.ons.validation.serverless;
 
-import java.util.Map;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import uk.gov.ons.validation.entity.InputData;
 import uk.gov.ons.validation.entity.OutputData;
 import uk.gov.ons.validation.service.ValueComparison;
 
-public class Handler implements RequestHandler<Map<String, Object>, ApiGatewayResponse> {
+public class Handler implements RequestHandler<InputData, OutputData> {
 
     private static final Logger LOG = LogManager.getLogger(Handler.class);
 
     @Override
-    public ApiGatewayResponse handleRequest(Map<String, Object> input, Context context) {
+    public OutputData handleRequest(InputData input, Context context) {
         LOG.info("received: {}", input);
 
         OutputData response = null;
 
         try {
-            response = new ValueComparison().apply(input.get("body").toString());
-            LOG.info("Returned from validation function: " + response.toString());
+            response = new ValueComparison().apply(input);
+            LOG.info("Returned from validation function: " + response);
         } catch (Exception e) {
             LOG.error(e.getStackTrace());
         }
 
-        return ApiGatewayResponse.builder()
-                .setStatusCode(200)
-                .setObjectBody(response)
-                .build();
+        return response;
     }
 }
--- src/main/java/uk/gov/ons/validation/service/ValueComparison.java
+++ src/main/java/uk/gov/ons/validation/service/ValueComparison.java
@@ -2,8 +2,6 @@ package uk.gov.ons.validation.service;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import uk.gov.ons.validation.entity.InputData;
 import uk.gov.ons.validation.entity.OutputData;
 
@@ -11,25 +9,9 @@ public class ValueComparison {
 
     private static final Logger LOG = LogManager.getLogger(ValueComparison.class);
 
-    public OutputData apply(String input) {
+    public OutputData apply(InputData input) {
         LOG.info("received: {}", input);
-        return parseAndRunValidationRule(input);
-    }
-
-    // Take any required source JSON, parse it into our data class, run the validation rule and then produce the output
-    private OutputData parseAndRunValidationRule(String inputJson) {
-        OutputData outputData;
-        try {
-            InputData inputData = new ObjectMapper().readValue(inputJson, InputData.class);
-            outputData = runValidationRule(inputData);
-        } catch (JsonProcessingException e) {
-            LOG.error("json error: {}", e.getMessage());
-            outputData = new OutputData(null, null, null, "Error parsing source JSON: " + inputJson);
-        } catch (Exception e) {
-            LOG.error("json error: {}", e.getMessage());
-            outputData = new OutputData(null, null, null, "Miscellaneous error parsing JSON input parameters: " + inputJson);
-        }
-        return outputData;
+        return runValidationRule(input);
     }
 
     // Take the given data and invoke the validation rule
